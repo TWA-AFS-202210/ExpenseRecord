9ad5769bc8f526c6fcbe84f221b23e0e5bb673c4
Fengjun Ma <FMa6@slb.com>
2022-10-19 13:55:32 +0800

    feat: add data dto

---
+++ ExpenseRecord/Dto/ConsumeRecordDto.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsumeRecord
+{
+    public class ConsumeRecord
+    {
+        public string? Id { get; set; }
+
+        public string? description { get; set; }
+
+        public string? type { get; set; }
+
+        public int? amount { get; set; }
+
+        public DateTime? date { get; set; }
+    }
+}
