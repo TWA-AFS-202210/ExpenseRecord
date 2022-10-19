642164afd4f1a6f414db1b7dfdbbda982a9e5aa7
Jie Li <JLi147@slb.com>
2022-10-19 13:53:56 +0800

    feat: Add ExpenseRecordController

---
+++ ExpenseRecord/Controllers/ExpenseRecordController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseRecord.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ExpenseRecordController : ControllerBase
+    {
+        [HttpGet]
+        public string greet()
+        {
+            return "Hello World";
+        }
+    }
+}
